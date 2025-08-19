6abc524209f9f3a93912d0c6db892cac287e457a
Jawavk <jawahar@swomb.app>
2025-08-20 01:18:28 +0530

    Truklynk Service

---
+++ goodone-common/src/main/java/com/goodone/common/util/ConfigConstants.java
@@ -0,0 +1,79 @@
+package com.goodone.common.util;
+
+public class ConfigConstants {
+
+	public static String DATABASE_PG_NAME = "database.pg.name";
+	public static String DATABASE_PG_USERNAME = "database.pg.username";
+	public static String DATABASE_PG_PASSWORD = "database.pg.password";
+	public static String DATABASE_PG_CONN_POOL_SIZE = "database.pg.conn.pool.size";
+	
+	public static String MICRO_SERVICE_PORT = "micro.service.port";
+	public static String MICRO_SERVICE_HOST = "micro.service.host";
+	public static String MICRO_SERVICE_CLIENT = "micro.service.client";
+	public static String MICRO_SERVICE_DB_SCHEMA = "micro.service.db.schema";
+	
+	public static String API_GATEWAY_HOST = "api.gateway.host";
+	public static String API_GATEWAY_ADMIN_PORT = "api.gateway.admin.port";
+	public static String API_GATEWAY_API_PORT = "api.gateway.api.port";
+	
+	public static String REQUEST_CONTENT_TYPE = "request.content.type";
+	public static String REQUEST_ACCESS_CONTROL_ALLOW_ORIGIN = "request.access.control.allow.origin";
+	public static String PUSH_GCM_CONTENT_TYPE = "push.gcm.content.type";
+	public static String PUSH_GCM_REQUEST_METHOD = "push.gcm.request.method";
+	public static String PUSH_GCM_AUTH_KEY_ANDROID = "push.gcm.auth.key.android";
+	public static String PUSH_GCM_AUTH_KEY = "push.gcm.auth.key";
+	public static String PUSH_GCM_PUSH_SERVICE_FILE = "push.gcm.push.service.file";
+	public static String PUSH_GCM_URL_STRING = "push.gcm.url.string";
+	public static String MAIL_CLIENT = "mail.client";
+	public static String GOOGLE_MAIL_KEY = "google.mail.key";
+	public static String GOOGLE_MAIL_ID = "google.mail.id";
+	public static String MICROSOFT_MAIL_KEY = "microsoft.mail.key";
+	public static String MICROSOFT_MAIL_ID = "microsoft.mail.id";
+	public static String AZURE_BLOB_CS = "azure.blob.cs";
+	public static String AZURE_BLOB_CONTAINER_NAME = "azure.blob.container.name";
+	public static String CRYPTO_KEYSTORE_FILE_NAME = "crypto.keystore.file.name";
+	public static String CRYPTO_FILE_KEY_PASSWORD = "crypto.file.key.password";
+	public static String CRYPTO_FILE_KEY_ALIAS_NAME = "crypto.file.key.alias.name";
+	public static String CRYPTO_KEYSTORE_TYPE = "crypto.keystore.type";
+	public static String CRYPTO_CRYPTOGRAPHY_ALGORITHM = "crypto.cryptography.algorithm";
+	public static String CRYPTO_MASTER_KEY_FILE_NAME = "crypto.master.key.file.name";
+	public static String DATABASE_PG_HOST = "database.pg.host";
+	public static String DATABASE_PG_PORT = "database.pg.port";
+
+	public static String FCM_NOTIFICATION_SCOPES = "fcm.notification.scopes";
+	public static String FCM_NOTIFICATION_CLASS_NAME = "fcm.notification.class.name";
+	public static String FCM_JSON_FILE_NAME = "fcm.json.file.name";
+	public static String FCM_END_POINT = "fcm.end.point";
+
+	public static String MEDIA_SERVICE_PORT = "media.service.port";
+	public static String MEDIA_SERVICE_HOST = "media.service.host";
+	public static String MEDIA_SERVICE_ENDPOINT_ONE = "media.service.endpoint.one";
+	public static String MEDIA_SERVICE_ENDPOINT_TWO = "media.service.endpoint.two";
+	public static String MEDIA_SERVICE_SSL = "media.service.ssl";
+
+	public static String EMAIL_SERVICE_PORT = "email.service.port";
+	public static String EMAIL_SERVICE_HOST = "email.service.host";
+	public static String EMAIL_SERVICE_ENDPOINT = "email.service.endpoint";
+	public static String EMAIL_SERVICE_SSL = "email.service.ssl";
+
+	public static String NOTIFICATION_SERVICE_PORT = "notification.service.port";
+	public static String NOTIFICATION_SERVICE_HOST = "notification.service.host";
+	public static String NOTIFICATION_SERVICE_ENDPOINT = "notification.service.endpoint";
+	public static String NOTIFICATION_SERVICE_SSL = "notification.service.ssl";
+
+	public static String FILEUPLOAD_SERVICE_PORT = "fileupload.service.port";
+	public static String FILEUPLOAD_SERVICE_HOST = "fileupload.service.host";
+	public static String FILEUPLOAD_SERVICE_ENDPOINT = "fileupload.service.endpoint";
+	public static String FILEUPLOAD_SERVICE_SSL = "fileupload.service.ssl";
+
+
+	public static String CONVERTOR_SERVICE_PORT = "convertor.service.port";
+	public static String CONVERTOR_SERVICE_HOST = "convertor.service.host";
+	public static String CONVERTOR_SERVICE_ENDPOINT = "convertor.service.endpoint";
+	public static String CONVERTOR_SERVICE_SSL = "convertor.service.ssl";
+	
+	
+	public static String BLOB_SERVICE_CONN_ENDPOINT = "blob.service.conn.endpoint";
+	public static String BLOB_SERVICE_CONTAINER = "blob.service.container";
+
+}
